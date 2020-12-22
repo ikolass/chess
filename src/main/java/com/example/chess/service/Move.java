8c71814b69ca017f75fac2418f3d2248e67fa3da
İlknur Aşkar <38427078+ikolass@users.noreply.github.com>
2020-12-22 05:58:02 +0300

    22.12.2020

--- src/main/java/com/example/chess/service/Move.java
+++ src/main/java/com/example/chess/service/Move.java
@@ -3,7 +3,6 @@ package com.example.chess.service;
 import com.example.chess.dto.Piece;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,108 +20,106 @@ public class Move {
         List<List<Piece>> chessBoard = chessService.createBoard();
         chessBoard.get(i).get(j);
         chessService.createBoard();
-                    Scanner scanner = new Scanner(System.in);
-                    System.out.println("Yapmak istediğiniz hamleyi seçiniz");
-                    int horse = scanner.nextInt();
-                    switch (horse) {
-                        case 1:
-                            z = x + 2;
-                            t = y + 1;
-                            if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
-                                String tempName = chessBoard.get(x).get(y).getName();
-                                String tempColor = chessBoard.get(x).get(y).getColor();
-                                chessBoard.get(x).get(y).setName(null);
-                                chessBoard.get(x).get(y).setColor(null);
-                                chessBoard.get(z).get(t).setName(tempName);
-                                chessBoard.get(z).get(t).setColor(tempColor);
-
-                            }
-                            break;
-                        case 2:
-                            z = x + 2;
-                            t = y - 1;
-                            if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
-                                String tempName = chessBoard.get(x).get(y).getName();
-                                String tempColor = chessBoard.get(x).get(y).getColor();
-                                chessBoard.get(x).get(y).setName(null);
-                                chessBoard.get(x).get(y).setColor(null);
-                                chessBoard.get(z).get(t).setName(tempName);
-                                chessBoard.get(z).get(t).setColor(tempColor);
-                            }
-                            break;
-                        case 3:
-                            z = x - 2;
-                            t = y - 1;
-                            if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
-                                String tempName = chessBoard.get(x).get(y).getName();
-                                String tempColor = chessBoard.get(x).get(y).getColor();
-                                chessBoard.get(x).get(y).setName(null);
-                                chessBoard.get(x).get(y).setColor(null);
-                                chessBoard.get(z).get(t).setName(tempName);
-                                chessBoard.get(z).get(t).setColor(tempColor);
-                            }
-                            break;
-                        case 4:
-                            z = x - 2;
-                            t = y + 1;
-                            if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
-                                String tempName = chessBoard.get(x).get(y).getName();
-                                String tempColor = chessBoard.get(x).get(y).getColor();
-                                chessBoard.get(x).get(y).setName(null);
-                                chessBoard.get(x).get(y).setColor(null);
-                                chessBoard.get(z).get(t).setName(tempName);
-                                chessBoard.get(z).get(t).setColor(tempColor);
-                            }
-                            break;
-                        case 5:
-                            z = x - 1;
-                            t = y - 2;
-                            if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
-                                String tempName = chessBoard.get(x).get(y).getName();
-                                String tempColor = chessBoard.get(x).get(y).getColor();
-                                chessBoard.get(x).get(y).setName(null);
-                                chessBoard.get(x).get(y).setColor(null);
-                                chessBoard.get(z).get(t).setName(tempName);
-                                chessBoard.get(z).get(t).setColor(tempColor);
-                            }
-                            break;
-                        case 6:
-                            z = x - 1;
-                            t = y + 2;
-                            if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
-                                String tempName = chessBoard.get(x).get(y).getName();
-                                String tempColor = chessBoard.get(x).get(y).getColor();
-                                chessBoard.get(x).get(y).setName(null);
-                                chessBoard.get(x).get(y).setColor(null);
-                                chessBoard.get(z).get(t).setName(tempName);
-                                chessBoard.get(z).get(t).setColor(tempColor);
-                            }
-                            break;
-                        case 7:
-                            z = x + 1;
-                            t = y - 2;
-                            if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
-                                String tempName = chessBoard.get(x).get(y).getName();
-                                String tempColor = chessBoard.get(x).get(y).getColor();
-                                chessBoard.get(x).get(y).setName(null);
-                                chessBoard.get(x).get(y).setColor(null);
-                                chessBoard.get(z).get(t).setName(tempName);
-                                chessBoard.get(z).get(t).setColor(tempColor);
-                            }
-                            break;
-                        case 8:
-                            z = x + 1;
-                            t = y + 2;
-                            if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
-                                String tempName = chessBoard.get(x).get(y).getName();
-                                String tempColor = chessBoard.get(x).get(y).getColor();
-                                chessBoard.get(x).get(y).setName(null);
-                                chessBoard.get(x).get(y).setColor(null);
-                                chessBoard.get(z).get(t).setName(tempName);
-                                chessBoard.get(z).get(t).setColor(tempColor);
-                            }
-                            break;
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Yapmak istediğiniz hamleyi seçiniz");
+        int horse = scanner.nextInt();
+        switch (horse) {
+            case 1:
+                z = x + 2;
+                t = y + 1;
+                if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
+                    String tempName = chessBoard.get(x).get(y).getName();
+                    String tempColor = chessBoard.get(x).get(y).getColor();
+                    chessBoard.get(x).get(y).setName(null);
+                    chessBoard.get(x).get(y).setColor(null);
+                    chessBoard.get(z).get(t).setName(tempName);
+                    chessBoard.get(z).get(t).setColor(tempColor);
+                }
+            break;
+            case 2:
+                z = x + 2;
+                t = y - 1;
+                if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
+                    String tempName = chessBoard.get(x).get(y).getName();
+                    String tempColor = chessBoard.get(x).get(y).getColor();
+                    chessBoard.get(x).get(y).setName(null);
+                    chessBoard.get(x).get(y).setColor(null);
+                    chessBoard.get(z).get(t).setName(tempName);
+                    chessBoard.get(z).get(t).setColor(tempColor);
+                }
+            break;
+            case 3:
+                z = x - 2;
+                t = y - 1;
+                if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
+                    String tempName = chessBoard.get(x).get(y).getName();
+                    String tempColor = chessBoard.get(x).get(y).getColor();
+                    chessBoard.get(x).get(y).setName(null);
+                    chessBoard.get(x).get(y).setColor(null);
+                    chessBoard.get(z).get(t).setName(tempName);
+                    chessBoard.get(z).get(t).setColor(tempColor);
+                }
+            break;
+            case 4:
+                z = x - 2;
+                t = y + 1;
+                if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
+                    String tempName = chessBoard.get(x).get(y).getName();
+                    String tempColor = chessBoard.get(x).get(y).getColor();
+                    chessBoard.get(x).get(y).setName(null);
+                    chessBoard.get(x).get(y).setColor(null);
+                    chessBoard.get(z).get(t).setName(tempName);
+                    chessBoard.get(z).get(t).setColor(tempColor);
+                }
+            break;
+            case 5:
+                z = x - 1;
+                t = y - 2;
+                if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
+                    String tempName = chessBoard.get(x).get(y).getName();
+                    String tempColor = chessBoard.get(x).get(y).getColor();
+                    chessBoard.get(x).get(y).setName(null);
+                    chessBoard.get(x).get(y).setColor(null);
+                    chessBoard.get(z).get(t).setName(tempName);
+                    chessBoard.get(z).get(t).setColor(tempColor);
+                }
+            break;
+            case 6:
+                z = x - 1;
+                t = y + 2;
+                if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
+                    String tempName = chessBoard.get(x).get(y).getName();
+                    String tempColor = chessBoard.get(x).get(y).getColor();
+                    chessBoard.get(x).get(y).setName(null);
+                    chessBoard.get(x).get(y).setColor(null);
+                    chessBoard.get(z).get(t).setName(tempName);
+                    chessBoard.get(z).get(t).setColor(tempColor);
+                }
+            break;
+            case 7:
+                z = x + 1;
+                t = y - 2;
+                if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
+                    String tempName = chessBoard.get(x).get(y).getName();
+                    String tempColor = chessBoard.get(x).get(y).getColor();
+                    chessBoard.get(x).get(y).setName(null);
+                    chessBoard.get(x).get(y).setColor(null);
+                    chessBoard.get(z).get(t).setName(tempName);
+                    chessBoard.get(z).get(t).setColor(tempColor);
+                }
+            break;
+            case 8:
+                z = x + 1;
+                t = y + 2;
+                if (!chessBoard.get(i).get(j).getColor().equals(chessBoard.get(z).get(t).getColor())) {
+                    String tempName = chessBoard.get(x).get(y).getName();
+                    String tempColor = chessBoard.get(x).get(y).getColor();
+                    chessBoard.get(x).get(y).setName(null);
+                    chessBoard.get(x).get(y).setColor(null);
+                    chessBoard.get(z).get(t).setName(tempName);
+                    chessBoard.get(z).get(t).setColor(tempColor);
+                }
+            break;
         }
         return chessBoard;
     }
